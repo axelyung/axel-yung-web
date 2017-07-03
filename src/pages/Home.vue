8c98c46e621cd2d0fea789b2bf8b3d549eb97252
axelyung <axelyung@gmail.com>
2017-07-03 22:22:48 +0200

    home styling

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="body row">
   <div class="bg-img" :style="{ backgroundImage: 'url(' + imgPath(data.bgImg) + ')' }"></div>
-    <div class="col-md-6">
+    <div class="blurb col-md-6">
       {{ data.blurb }}
     </div>
   </div>
