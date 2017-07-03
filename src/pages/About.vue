545e83d314b51b0d4cee9c1266f65f08a93aedeb
axelyung <axelyung@gmail.com>
2017-07-03 13:24:37 +0200

    about view

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,13 +1,28 @@
 <template>
-  <div></div>
+  <div class="body">
+    <div class="row">
+      <div class="offset-lg-1 col-lg-4">
+        <img :src="imgPath(data.img)"></img>
+      </div>
+      <div class="col-lg-7">
+        <h1>{{ data.title }}</h1>
+        <div v-html="data.bio"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import site from 'site'
 export default {
-
+  data() {
+    return {
+      data: site.pages.about
+    }
+  }
 }
 </script>
 
 <style>
-  
+
 </style>
