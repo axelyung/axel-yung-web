3c015c70455d6dff6d2524ba535b6f31fdf086bb
axelyung <axelyung@gmail.com>
2017-07-03 22:20:12 +0200

    footer styling

--- src/components/Foot.vue
+++ src/components/Foot.vue
@@ -1,12 +1,14 @@
 <template>
   <footer class="footer">
-    <div class="title">{{ data.title }}</div>
-    <div class="email">{{ data.email }}</div>
-    <div class="tele-nr">{{ data.teleNr }}</div>
-    <div class="media-links align-center">
-      <a class="inline-block" v-for="(item,i) in data.media" :key="i" :href="item.url" target="_blank">
-        <svg-icon :icon="item.icon"></svg-icon>
-      </a>
+    <div class="inner">
+      <a class="title block" href="/">{{ data.title }}</a>
+      <a class="email block" :href="'mailto:' + data.email">{{ data.email }}</a>
+      <div class="tele-nr">{{ data.teleNr }}</div>
+      <div class="media-links align-center">
+        <a class="inline-block" v-for="(item,i) in data.media" :key="i" :href="item.url" target="_blank">
+          <svg-icon :icon="item.icon"></svg-icon>
+        </a>
+      </div>
     </div>
   </footer>
 </template>
@@ -16,14 +18,14 @@ import './icons';
 import data from 'site';
 
 export default {
-    data(){
-      return {
-        data: data.footer,
-      }
+  data() {
+    return {
+      data: data.footer,
     }
+  }
 }
 </script>
 
 <style>
-  
+
 </style>
