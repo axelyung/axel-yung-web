4048b884ff70d63847fa22b621a3b90c99fba007
axelyung <axelyung@gmail.com>
2017-07-03 13:24:28 +0200

    resources view

--- src/pages/Resources.vue
+++ src/pages/Resources.vue
@@ -1,13 +1,34 @@
 <template>
-  <div></div>
+  <div class="body">
+    <div class="row">
+      <div class="offset-md-3 col-md-6">
+        <h1>{{ data.title }}</h1>
+        <p>{{ data.blurb }}</p>
+      </div>
+    </div>
+    <div class="row">
+      <div v-for="(subject, i) in data.subjects" :key="i" class="col-md-3">
+        <div>{{ subject.name }}</div>
+          <a v-for="link in subject.links" :key="link.text" :href="link.url">
+            {{ link.text }}
+          </a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import site from 'site'
 
+export default {
+  data() {
+    return {
+      data: site.pages.resources
+    }
+  }
 }
 </script>
 
 <style>
-  
+
 </style>
